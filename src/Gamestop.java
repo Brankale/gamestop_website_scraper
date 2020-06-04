9b8b3f727f3e061e7623e10f0cac988044bf5a56
Brankale <45881310+Brankale@users.noreply.github.com>
2020-06-04 22:46:11 +0200

    add constructor and model classes

--- src/Gamestop.java
+++ src/Gamestop.java
@@ -1,7 +1,39 @@
 public class Gamestop {
 
-    public Gamestop() {
+    private String website = "www.gamestop";
 
+    public enum Country {
+        AUSTRIA,
+        ITALY,
+        SWITZERLAND,
+        DENMARK,
+        GERMANY,
+        NORWAY,
+        SWEDEN,
+        FINLAND,
+        IRELAND
+    }
+
+    public Gamestop(Country region) {
+        switch (region) {
+            case AUSTRIA:       website += ".at"; break;
+            case ITALY:         website += ".it"; break;
+            case SWITZERLAND:   website += ".ch"; break;
+            case DENMARK:       website += ".dk"; break;
+            case GERMANY:       website += ".de"; break;
+            case NORWAY:        website += ".no"; break;
+            case SWEDEN:        website += ".se"; break;
+            case FINLAND:       website += ".fi"; break;
+            case IRELAND:       website += ".ie"; break;
+        }
+    }
+
+    public void search() {
+        // TODO ("to implement")
+    }
+
+    public void findById() {
+        // TODO ("to implement")
     }
 
 }
