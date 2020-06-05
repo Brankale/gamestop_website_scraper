6c120ad9c3a3e80125319f824629ee25c7df0c3f
Brankale <45881310+Brankale@users.noreply.github.com>
2020-06-05 19:25:18 +0200

    add some methods and variables to GamePreview

--- src/models/GamePreview.java
+++ src/models/GamePreview.java
@@ -1,5 +1,57 @@
 package models;
 
+import java.util.Objects;
+
 public class GamePreview {
-    // TODO ("to implement")
+
+    private final int id;
+    private String title;
+    private String publisher;
+    private String platform;
+
+    public GamePreview(int id) {
+        this.id = id;
+    }
+
+    public final int getId() {
+        return id;
+    }
+
+    public final String getTitle() {
+        return title;
+    }
+
+    public final void setTitle(String title) {
+        this.title = title;
+    }
+
+    public final String getPublisher() {
+        return publisher;
+    }
+
+    public final void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public final String getPlatform() {
+        return platform;
+    }
+
+    public final void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GamePreview that = (GamePreview) o;
+        return id == that.id;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
