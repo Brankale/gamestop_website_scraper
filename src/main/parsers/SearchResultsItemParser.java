3865461848bda25ac6759b6a7c69ff2bb9208a66
Brankale <45881310+Brankale@users.noreply.github.com>
2020-12-25 23:28:59 +0100

    created SearchResultsItemParser

---
+++ src/main/parsers/SearchResultsItemParser.java
@@ -0,0 +1,25 @@
+package main.parsers;
+
+import org.jsoup.nodes.Element;
+
+public class SearchResultsItemParser {
+
+    /**
+     * Returns the title of the game given the h3 tag
+     * @param rootTag h3 tag inside <div class="classProdInfo">
+     * @return the title of the game
+     */
+    public String parseTitle(Element rootTag) {
+        return rootTag.child(0).text().trim();
+    }
+
+    /**
+     * Returns the platform of the game given the h4 tag
+     * @param rootTag h4 tag inside <div class="classProdInfo">
+     * @return the platform of the game
+     */
+    public String parsePlatform(Element rootTag) {
+        return rootTag.textNodes().get(0).text().trim();
+    }
+
+}
---
+++ src/test/parsers/SearchResultsItemParserTest.java
@@ -0,0 +1,49 @@
+package test.parsers;
+
+import main.parsers.SearchResultsItemParser;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchResultsItemParserTest {
+
+    private static SearchResultsItemParser parser;
+
+    @BeforeAll
+    private static void initParser() {
+        parser = new SearchResultsItemParser();
+    }
+
+    private Element createElement(String html) {
+        return Jsoup.parse(html).body().child(0);
+    }
+
+    @Test
+    public void parseTitle() {
+        String title = "Persona® 5 Strikers";
+        String html =   "<h3>" +
+                        "   <a href=\"/Switch/Games/133354/persona-5-strikers\">" +
+                                title +
+                        "   </a>" +
+                        "</h3>";
+        Element element = createElement(html);
+        String parsedTitle = parser.parseTitle(element);
+        assertEquals(title, parsedTitle);
+    }
+
+    @Test
+    public void parsePlatform() {
+        String platform = "Switch";
+        String html =   "<h4 class=\"platLogo plat-Switch\">" +
+                            platform +
+                            "<span class=\"hideOnMobile\"> by <strong>Atlus</strong> </span>" +
+                        "</h4>";
+        Element element = createElement(html);
+        String parsedPlatform = parser.parsePlatform(element);
+        assertEquals(platform, parsedPlatform);
+    }
+
+}
