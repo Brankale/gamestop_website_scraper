ff4a34d61e5aa1d93980f1d9af60507f4ddbcb57
Brankale <45881310+Brankale@users.noreply.github.com>
2021-01-10 22:21:21 +0100

    removed dead code

--- src/main/parsers/SearchResultsParser.java
+++ src/main/parsers/SearchResultsParser.java
@@ -1,40 +1,19 @@
 package main.parsers;
 
 import com.sun.istack.internal.NotNull;
-import main.models.GamePreview;
-import main.models.GamePreviews;
 import main.models.price.Price;
 import main.models.price.PriceType;
 import main.models.price.Prices;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class SearchResultsParser {
 
     @NotNull
-    public static GamePreviews getSearchResults(@NotNull final String link) throws IOException {
-
-        final Document root = Jsoup.connect(link).get();
-
-        final GamePreviews gamePreviews = new GamePreviews();
-
-        Elements items = getItems(root);
-        for (Element item : items) {
-            final GamePreview gamePreview = parseItem(item);
-            gamePreviews.add(gamePreview);
-        }
-
-        return gamePreviews;
-    }
-
-    @NotNull
-    private static Elements getItems(@NotNull Document root) {
+    private static Elements getItems(@NotNull final Element root) {
         final Element productsList = root.getElementById("productsList");
         if (productsList != null) {
             return productsList.getElementsByClass("singleProduct");
@@ -43,48 +22,7 @@ public class SearchResultsParser {
         }
     }
 
-    @NotNull
-    private static GamePreview parseItem(@NotNull final Element item) {
-
-        final int id = getIdFromItem(item);
-        final GamePreview gamePreview = new GamePreview(id);
-
-        final String title = getTitleFromItem(item);
-        final String platform = getPlatformFromItem(item);
-        final String publisher = getPublisherFromItem(item);
-        final Prices prices = getPricesFromItem(item);
-
-        gamePreview.setTitle(title);
-        gamePreview.setPlatform(platform);
-        gamePreview.setPublisher(publisher);
-        gamePreview.addPrices(prices);
-
-        // TODO: init gamepreview parameters
-
-        return gamePreview;
-    }
-
-    private static int getIdFromItem(@NotNull final Element item) {
-        final Element prodImg = item.getElementsByClass("prodImg").first();
-        final String id = prodImg.attr("href").split("/")[3];
-        return Integer.parseInt(id);
-    }
-
-    @NotNull
-    private static String getTitleFromItem(@NotNull final Element item) {
-        return item.getElementsByTag("h3").first().text();
-    }
-
-    @NotNull
-    private static String getPlatformFromItem(@NotNull final Element item) {
-        return item.getElementsByTag("h4").first().textNodes().get(0).text().trim();
-    }
-
-    @NotNull
-    private static String getPublisherFromItem(@NotNull final Element item) {
-        return item.getElementsByTag("h4").first().getElementsByTag("strong").text();
-    }
-
+    // TODO: price parsing should be done in a separate class
     @NotNull
     private static Prices getPricesFromItem(@NotNull final Element item) {
         final Prices prices = new Prices();
@@ -102,6 +40,7 @@ public class SearchResultsParser {
         return prices;
     }
 
+    // TODO: price parsing should be done in a separate class
     @NotNull
     private static Price getPrice(final Element priceType, final Element availability) {
 
@@ -164,6 +103,7 @@ public class SearchResultsParser {
         return price;
     }
 
+    // TODO: price parsing should be done in a separate class
     private static float stringToPrice(@NotNull final String price) {
 
         String str = price;
--- src/test/parsers/SearchResultsParserTest.java
+++ src/test/parsers/SearchResultsParserTest.java
@@ -1,63 +1,23 @@
 package test.parsers;
 
-import main.models.GamePreview;
-import main.models.GamePreviews;
-import main.models.price.Price;
-import main.models.price.Prices;
-import main.parsers.SearchResultsParser;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.junit.jupiter.api.Test;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.math.BigDecimal;
+import org.junit.jupiter.api.Disabled;
 
 class SearchResultsParserTest {
 
-    @Test
+    // TODO: this test should be moved into Gamestop class tests
+    @Disabled
     public void skipGiftCard() {
-        final String link = "https://www.gamestop.it/SearchResult/QuickSearch?q=giftcard";
-
-        try {
-            SearchResultsParser.getSearchResults(link);
-        } catch (IOException ex) {
-            assert true;
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            assert false;
-        }
-
-        assert true;
-    }
-
-    // Use this to test new features
-    @Test
-    public void newFeature() {
-        final String link = "https://www.gamestop.it/SearchResult/QuickSearch?q=persona";
-
-        try {
-            GamePreviews gamePreviews = SearchResultsParser.getSearchResults(link);
-
-            for (GamePreview gamePreview : gamePreviews) {
-                System.out.println("\ntitle: " + gamePreview.getTitle());
-                Prices prices = gamePreview.getPrices();
-                if (!prices.isEmpty()) {
-                    for (Price price : prices) {
-                        System.out.println("type: " + price.getType().toString() );
-                        System.out.println("price: " + price.getPrice());
-
-                        for (BigDecimal oldPrice : price.getOldPrices()) {
-                            System.out.println("oldPrice: " + oldPrice);
-                        }
-                    }
-                }
-            }
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+//        final String link = "https://www.gamestop.it/SearchResult/QuickSearch?q=giftcard";
+//
+//        try {
+//            SearchResultsParser.getSearchResults(link);
+//        } catch (IOException ex) {
+//            assert true;
+//        } catch (ArrayIndexOutOfBoundsException ex) {
+//            assert false;
+//        }
+//
+//        assert true;
     }
 
 }
