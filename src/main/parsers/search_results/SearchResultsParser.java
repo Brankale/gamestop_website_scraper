28d9d0e3890419edd47b653b149fa7d0a60779e1
Brankale <45881310+Brankale@users.noreply.github.com>
2021-02-19 11:11:54 +0100

    search results parser: added javadoc

--- src/main/parsers/search_results/SearchResultsParser.java
+++ src/main/parsers/search_results/SearchResultsParser.java
@@ -7,11 +7,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-// TODO: add documentation
+/**
+ * Given a valid HTML representing a GameStop search
+ * results page, it returns the results found.
+ * A valid HTML has one tag with id = "productsList".
+ */
 public class SearchResultsParser {
 
+    /**
+     * @param root org.jsoup.nodes.Document representing
+     *             the HTML search results page
+     * @return GamePreviews
+     */
     @NotNull
-    public static GamePreviews parse(Document root) {
+    public static GamePreviews parse(@NotNull Document root) {
         Elements items = getItems(root);
         GamePreviews gamePreviews = new GamePreviews();
         for (Element item : items) {
@@ -21,8 +30,14 @@ public class SearchResultsParser {
         return gamePreviews;
     }
 
+    /**
+     * @param root org.jsoup.nodes.Document representing
+     *             the HTML search results page
+     * @return Elements representing search results
+     * @throws InvalidHtmlException if tag with id = "productsList" is missing
+     */
     @NotNull
-    private static Elements getItems(@NotNull final Document root) {
+    private static Elements getItems(@NotNull Document root) {
         Element productsList = root.getElementById("productsList");
         if (productsList == null) {
             throw new InvalidHtmlException(root);
