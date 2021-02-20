bd268d3e724a4745acfe829252ab1242806cb534
Brankale <45881310+Brankale@users.noreply.github.com>
2021-02-20 21:43:18 +0100

    gamestop: added first implementation of search method

--- src/main/Gamestop.java
+++ src/main/Gamestop.java
@@ -1,5 +1,12 @@
 package main;
 
+import main.models.GamePreviews;
+import main.parsers.search_results.SearchResultsParser;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Gamestop {
 
     private String website = "www.gamestop";
@@ -30,8 +37,19 @@ public class Gamestop {
         }
     }
 
-    public void search() {
-        // TODO ("to implement")
+    public GamePreviews search(String searched) {
+        // TODO: throw a custom exception on IOException
+        try {
+            Document html = Jsoup.connect(getSearchQuery(searched)).get();
+            return SearchResultsParser.parse(html);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new GamePreviews();
+    }
+
+    private String getSearchQuery(String searched) {
+        return website + "?q=" + searched;
     }
 
     public void findById() {
