2fc6699f844e101dae90dc2804e08299674d3b18
Brankale <45881310+Brankale@users.noreply.github.com>
2021-03-11 18:56:33 +0100

    implemented GameParser skeleton

--- src/main/parsers/game_page/GamePageParser.java
+++ src/main/parsers/game_page/GamePageParser.java
@@ -1,9 +1,80 @@
 package main.parsers.game_page;
 
+import main.models.Game;
+import main.models.price.Prices;
+import org.jsoup.nodes.Element;
+
+import java.util.List;
+
 public class GamePageParser {
 
     private GamePageParser() {
         // prevent instantiation
+    }
+
+    private static String getTitle(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static Prices getPrices(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static String getPlatform(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static String getPublisher(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static String getReleaseDate(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static String getCoverUrl(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static List<String> getGallery(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static List<Game.Pegi> getPegi(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static List<String> getGenres(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static String getOfficialSite(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static String getPlayers(Element element) {
+        // TODO: to implement
+        return null;
+    }
+
+    private static boolean isValidForPromotions(Element element) {
+        // TODO: to implement
+        return false;
+    }
+
+    private static String getDescription(Element element) {
+        // TODO: to implement
+        return null;
     }
 
 }
